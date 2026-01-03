a53f5dda8b12d2e161c4bd6050c01f42dc0c6a5a
Alexandre <aalcarvalho@celepar.pr.gov.br>
2026-01-02 22:38:12 -0300

    Hello World simples

---
+++ day-001/src/App/Main.java
@@ -0,0 +1,26 @@
+package App;
+public class Main{
+
+	public static void main(String[] args){
+		Main principal = new Main();
+		principal.inicia();
+	}
+
+	private void inicia(){
+		System.out.println("Programa inicial");
+		int a = 10;
+		String nome = "Sam";
+		byte b = 10;
+		char c = 'c';
+		long d = 1223215554L;
+		float e = 1.3f;
+		double f = 2.4;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(nome);
+	}
+}
